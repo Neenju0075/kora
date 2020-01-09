8dde59a4956062505d1fa9207b650bd40674e7d1
Niranjanan Harimohan <niranjanan.harimohan@freshworks.com>
2020-01-09 20:03:49 +0530

    added menus

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,11 @@
 module ApplicationHelper
   def full_title(page_title)
     if page_title.empty?
-      "What page ra ithi????"
+      "What page???? add title to the page!!"
     elsif page_title.downcase.include? "signup"
       link_to "Login", login_path, class:"navbar-item"
     elsif page_title.downcase.include? "login"
       link_to "Signup", new_user_path, class:"navbar-item"
-    elsif page_title.downcase.include? "question"
-      link_to "Create New Questions", new_question_path, class:"navbar-item"
     end
   end
 end
